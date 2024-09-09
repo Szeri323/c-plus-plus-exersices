8fe345c2eae276375387dbbac52dc7357b75d1db
Szeri <szeri323@gmail.com>
2024-09-09 15:40:07 +0200

    fix error handling

--- errorsHandling/errorsHandling/errorsHandling.cpp
+++ errorsHandling/errorsHandling/errorsHandling.cpp
@@ -1,36 +1,39 @@
 #include "../../../headers/std_lib_facilities.h"
 
 int area(int length, int width) {
-    if (length <= 0 || width <= 0) return -1;
+    cout << "area func called." << endl;
+    if (length <= 0 || width <= 0) error("Argument of function area() isn't positive.");
     return length * width;
 }
 
-int framed_area(int x, int y) {
-    if (x <= 2 || y <= 2) return -1;
-    return area(x - 2, y - 2);
+int framed_area(int length, int width) {
+    constexpr int frame_width = 2;
+    cout << "framed area func called." << endl;
+    if (length - frame_width <= 0 || width - frame_width <= 0) error("Argument of function area() called by framed_area() isn't positive.");
+    return area(length - frame_width, width - frame_width);
 }
 
 int f(int x, int y, int z)
 {
     int area1 = area(x, y);
-    if (area1 <= 0) return -1;
+    if (area1 <= 0) error("Area isn't positive.");
     int area2 = framed_area(1, z);
-    if (area2 == -1) return -1;
+    if (area2 <= 0) error("Framed area isn't positive.");
     int area3 = framed_area(y, z);
-    if (area1 == 0 || area3 == 0)
-        return -1;
-    double ratio = double(area1 / area3);
-    return int(ratio);
+    if (area3 <= 0) error("Framed area isn't positive.");
+    return double(area1 / area3);
 }
 
 int main() {
     int x, y, z;
     while (cin >> x >> y >> z) {
-        int result = f(x, y, z);
-        if (result == -1)
-            cout << "Something went wrong.";
-        else {
-            cout << "F: " << result << endl;
+        try
+        {
+            cout << "F: " << f(x, y, z) << endl;
+        }
+        catch (runtime_error& e)
+        {
+            cout << "Something went wrong: " << e.what() << endl;
         }
     }
 }
