12427e342105f0d598818e69f5fbedab8aa3cb3f
Szeri <szeri323@gmail.com>
2024-09-17 21:11:14 +0200

    add class to nameAge

--- 21.nameAge/nameAge/nameAge.cpp
+++ 21.nameAge/nameAge/nameAge.cpp
@@ -1,7 +1,95 @@
 #include "../../../headers/std_lib_facilities.h"
 
+class Name_value {
+public:
+	void set(string name, int age) {
+		this->name = name;
+		this->age = age;
+	}
+	string getName() {
+		return name;
+	}
+	int getAge() {
+		return age;
+	}
+private:
+	string name;
+	int age;
+};
+
 int main() {
-	vector<string> names;
+	vector<Name_value> v;
+	string name;
+	int age;
+	bool flag = false;
+	while (cin >> name >> age) {
+		if (name == "Nosurname" && age == 0) break;
+		Name_value p;
+		p.set(name, age);
+		for (Name_value x : v) {
+			if (x.getName() == name) error("We have duplicate in names.");
+		}
+		v.push_back(p);
+	}
+
+	for (Name_value x : v) {
+		cout << x.getName() << " " << x.getAge() << endl;
+	}
+
+	bool next = false;
+	name = "";
+	cout << "Do you want check some names?" << endl;
+	cin >> next;
+	if (next) {
+		while (cin >> name) {
+			bool found = false;
+			if (name == "Noname") {
+				break;
+			}
+			for (Name_value x : v) {
+				if (x.getName() == name) {
+					cout << x.getAge() << endl;
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				cout << "Name not found." << endl;
+			}
+		}
+	}
+	else {
+		return 0;
+	}
+	age = 0;
+	cout << "Do you want check some ages?" << endl;
+	cin >> next;
+	if (next) {
+		while (cin >> age) {
+			bool found = false;
+			if (age < 1) {
+				break;
+			}
+			for (Name_value x : v) {
+				if (x.getAge() == age) {
+					cout << x.getName() << endl;
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				cout << "Age not found." << endl;
+			}
+		}
+	}
+	else {
+		return 0;
+	}
+
+
+
+
+	/*vector<string> names;
 	vector<int> ages;
 	string temp_s;
 	int temp_i;
@@ -65,5 +153,5 @@ int main() {
 	}
 	else {
 		return 0;
-	}
+	}*/
 }
