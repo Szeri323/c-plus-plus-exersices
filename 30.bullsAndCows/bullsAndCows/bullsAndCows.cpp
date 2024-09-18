bf6fbc759d4341dd8996a4577d8337531ed6e13a
Szeri <szeri323@gmail.com>
2024-09-18 16:32:17 +0200

    add letters for bulls and cows

--- 30.bullsAndCows/bullsAndCows/bullsAndCows.cpp
+++ 30.bullsAndCows/bullsAndCows/bullsAndCows.cpp
@@ -1,5 +1,7 @@
 #include "../../../headers/std_lib_facilities.h"
 
+// Bull and Cows numbers
+
 int char_to_digit(char c) {
 	switch (c) {
 	case '0':
@@ -44,6 +46,7 @@ vector<int> populte_vector() {
 	}
 	return solution;
 }
+
 vector<int> create_vector_from_string(string choice) {
 	vector<int> choice_numbers;
 	for (int i = 0; i < choice.size(); ++i) {
@@ -51,6 +54,7 @@ vector<int> create_vector_from_string(string choice) {
 	}
 	return choice_numbers;
 }
+
 
 bool compare_vectors(vector<int> solution, vector<int> choice_numbers) {
 	int bulls = 0;
@@ -76,43 +80,152 @@ bool compare_vectors(vector<int> solution, vector<int> choice_numbers) {
 	cout << "Bulls: " << bulls << " " << "Cows: " << cows << endl;
 }
 
-int main() {
-	while (true) {
-		int n = 0;
-		cout << "Pass randomly choosen number: " << endl;
-		cin >> n;
-		seed_randint(n);
-		vector<int> solution = populte_vector();
-		string choice;
-		int counter = 0;
-		bool again = false;
-		while (true) {
-			try {
-				cout << "Write 4 numbers from 0 to 9: " << endl;
-				cin >> choice;
-				if (choice.size() < 4 || choice.size() > 4) error("Incorrect number of numbers. Pass 4.");
-				vector<int> choice_numbers = create_vector_from_string(choice);
-				++counter;
-				if (compare_vectors(solution, choice_numbers) == true) {
-					if (counter == 1) {
-						cout << "You win in " << counter << " attempt." << endl;
-					}
-					else {
-						cout << "You win in " << counter << " attempts." << endl;
-					}
-					cout << "Do you want play again?" << endl;
-					cin >> again;
-					break;
+// Bull and Cows letters
 
+vector<char> populte_vector_char() {
+	vector<char> solution;
+	while (solution.size() != 4) {
+		int random = randint(97, 122);
+		bool flag = false;
+		if (solution.size() == 0) {
+			solution.push_back(char(random));
+		}
+		for (int j = 0; j < solution.size(); ++j) {
+			if (random == solution[j])
+				flag = true;
+		}
+		if (flag == false)
+			solution.push_back(random);
+	}
+	return solution;
+}
+
+vector<char> create_vector_char_from_string(string choice) {
+	vector<char> choice_letters;
+	for (int i = 0; i < choice.size(); ++i) {
+		choice_letters.push_back(choice[i]);
+	}
+	return choice_letters;
+}
+
+bool compare_vectors_char(vector<char> solution, vector<char> choice_numbers) {
+	int bulls = 0;
+	int cows = 0;
+	for (int i = 0; i < solution.size(); ++i) {
+		if (solution[i] == choice_numbers[i]) {
+			bulls += 1;
+		}
+		else {
+			for (int j = 0; j < solution.size(); ++j) {
+				if (j == i) {
+					continue;
+				}
+				if (choice_numbers[i] == solution[j]) {
+					cows += 1;
 				}
 			}
-			catch (exception& e) {
-				cout << e.what() << endl;
+		}
+		if (bulls == 4) {
+			return true;
+		}
+	}
+	cout << "Bulls: " << bulls << " " << "Cows: " << cows << endl;
+}
+
+int main() {
+	int choice;
+	cout << "Do you want play with letters (1) or digits (2)?" << endl;
+	cin >> choice;
+	if (choice == 1) {
+		while (true) {
+			int n = 0;
+			cout << "Pass randomly choosen number: " << endl;
+			cin >> n;
+			seed_randint(n);
+			vector<char> solution = populte_vector_char();
+			string choice;
+			int counter = 0;
+			bool again = false;
+			while (true) {
+				try {
+					cout << "Write 4 letter from a to z: " << endl;
+					cin >> choice;
+					if (choice.size() < 4 || choice.size() > 4) error("Incorrect number of letters. Pass 4.");
+					for (int i = 0; i < choice.size(); ++i) {
+						for (int j = 0; j < choice.size(); ++j) {
+							if (j == i) continue;
+							if (choice[j] == choice[i]) error("letters must be different.");
+						}
+					}
+					vector<char> choice_numbers = create_vector_char_from_string(choice);
+					++counter;
+					if (compare_vectors_char(solution, choice_numbers) == true) {
+						if (counter == 1) {
+							cout << "You win in " << counter << " attempt." << endl;
+						}
+						else {
+							cout << "You win in " << counter << " attempts." << endl;
+						}
+						cout << "Do you want play again?" << endl;
+						cin >> again;
+						break;
+
+					}
+				}
+				catch (exception& e) {
+					cout << e.what() << endl;
+				}
+			}
+			if (again == false) {
+				cout << "Thanks for play, see you next time." << endl;
+				break;
 			}
 		}
-		if (again == false) {
-			cout << "Thanks for play, see you next time." << endl;
-			break;
+	}
+	else if (choice == 2) {
+		while (true) {
+			int n = 0;
+			cout << "Pass randomly choosen number: " << endl;
+			cin >> n;
+			seed_randint(n);
+			vector<int> solution = populte_vector();
+			string choice;
+			int counter = 0;
+			bool again = false;
+			while (true) {
+				try {
+					cout << "Write 4 numbers from 0 to 9: " << endl;
+					cin >> choice;
+					if (choice.size() < 4 || choice.size() > 4) error("Incorrect number of numbers. Pass 4.");
+					for (int i = 0; i < choice.size(); ++i) {
+						for (int j = 0; j < choice.size(); ++j) {
+							if (j == i) continue;
+							if (choice[j] == choice[i]) error("Numbers must be different.");
+						}
+					}
+					vector<int> choice_numbers = create_vector_from_string(choice);
+					++counter;
+					if (compare_vectors(solution, choice_numbers) == true) {
+						if (counter == 1) {
+							cout << "You win in " << counter << " attempt." << endl;
+						}
+						else {
+							cout << "You win in " << counter << " attempts." << endl;
+						}
+						cout << "Do you want play again?" << endl;
+						cin >> again;
+						break;
+
+					}
+				}
+				catch (exception& e) {
+					cout << e.what() << endl;
+				}
+			}
+			if (again == false) {
+				cout << "Thanks for play, see you next time." << endl;
+				break;
+			}
 		}
 	}
 }
