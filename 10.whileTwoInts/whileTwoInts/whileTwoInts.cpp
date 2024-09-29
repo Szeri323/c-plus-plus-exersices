82a242cd89c77bae147fa1d3c3e177e5b59d1f66
Szeri <szeri323@gmail.com>
2024-09-29 21:10:31 +0200

    update while two ints logic

--- 10.whileTwoInts/whileTwoInts/whileTwoInts.cpp
+++ 10.whileTwoInts/whileTwoInts/whileTwoInts.cpp
@@ -1,5 +1,19 @@
 #include "../../../headers/std_lib_facilities.h"
 
+void print_instruction() {
+	cout << "Pass value and unit to convert to end use '|':" << endl;
+}
+
+void print_results(vector<double> metrics, double min, double max, double sum) {
+	cout << "Size of the metrics: " << metrics.size() << "\n";
+	cout << "Sum of the metrics: " << sum << 'm' << "\n";
+	cout << "Minimum of the metrics: " << min << 'm' << "\n";
+	cout << "Maximum of the metrics: " << max << 'm' << "\n";
+	sort(metrics.begin(), metrics.end());
+	for (double x : metrics)
+		cout << x << " ";
+}
+
 double conversion(double value, string unit) {
 	constexpr double cm_to_m = 0.01;
 	constexpr double cal_to_m = 0.0254;
@@ -17,23 +31,18 @@ double conversion(double value, string unit) {
 		return value * st_to_m;
 	}
 	else {
-		return -1;
+		error("Invalid unit");
 	}
 }
 
-int main() {
+void calculate() {
 	vector<double> metrics;
 	double min = 0, max = 0, value = 0, sum = 0;
 	string unit;
 	bool flag = true;
-	cout << "Pass value and unit to convert to end use '|':" << endl;
 	while (cin >> value >> unit) {
 		cout << value << unit << "\n";
 		value = conversion(value, unit);
-		if (value == -1) {
-			cout << "Invalid unit";
-			return 0;
-		}
 		metrics.push_back(value);
 		sum += value;
 		cout << value << "m" << "\n";
@@ -41,6 +50,7 @@ int main() {
 			min = value;
 			max = value;
 			flag = false;
+			cout << "This number is minimum and maximum now." << "\n";
 		}
 		if (value < min) {
 			min = value;
@@ -49,13 +59,19 @@ int main() {
 		if (value > max) {
 			max = value;
 			cout << "This number is maximum now." << "\n";
-		}		
+		}
 	}
-	cout << "Size of the metrics: " << metrics.size() << "\n";
-	cout << "Sum of the metrics: " << sum << 'm' << "\n";
-	cout << "Minimum of the metrics: " << min << 'm' << "\n";
-	cout << "Maximum of the metrics: " << max << 'm' << "\n";
-	sort(metrics.begin(), metrics.end());
-	for (double x : metrics)
-		cout << x << " ";
+	print_results(metrics, sum, min, max);
+}
+
+
+
+int main() {
+	try {
+		print_instruction();
+		calculate();
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
 }
