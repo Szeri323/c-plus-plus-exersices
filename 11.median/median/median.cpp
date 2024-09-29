abaef4dfbbec73edd2787905d69d93d79e588c7a
Szeri <szeri323@gmail.com>
2024-09-29 20:57:15 +0200

    update median

--- 11.median/median/median.cpp
+++ 11.median/median/median.cpp
@@ -1,6 +1,6 @@
 #include "../../../headers/std_lib_facilities.h"
 
-int main() {
+double calculate_median() {
 	vector<double> temperatures;
 	double temp;
 	while (cin >> temp) {
@@ -8,9 +8,19 @@ int main() {
 	}
 	sort(temperatures.begin(), temperatures.end());
 	if (temperatures.size() % 2 == 0) {
-		cout << "Mediana: " << (temperatures[temperatures.size() / 2] + temperatures[temperatures.size() / 2 -1]) /2 << endl;
+		return (temperatures[temperatures.size() / 2] + temperatures[temperatures.size() / 2 - 1]) / 2;
 	}
 	else {
-		cout << "Mediana: " << temperatures[temperatures.size() / 2] << endl;
+		return temperatures[temperatures.size() / 2];
 	}
+}
+
+int main() {
+	try {
+		cout << "Median: " << calculate_median() << endl;
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+	
 }
