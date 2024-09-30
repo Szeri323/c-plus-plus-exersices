c64de69ab3e169a46a83feb4a78099a204a42abc
Szeri <szeri323@gmail.com>
2024-09-30 14:16:08 +0200

    add try catch and update logic of name age program

--- 21.nameAge/nameAge/nameAge.cpp
+++ 21.nameAge/nameAge/nameAge.cpp
@@ -2,10 +2,7 @@
 
 class Name_value {
 public:
-	void set(string name, int age) {
-		this->name = name;
-		this->age = age;
-	}
+	Name_value(string name, int age) : name(name), age(age) {}
 	string getName() {
 		return name;
 	}
@@ -17,15 +14,14 @@ private:
 	int age;
 };
 
-int main() {
+vector<Name_value> populate_vector() {
 	vector<Name_value> v;
 	string name;
 	int age;
 	bool flag = false;
 	while (cin >> name >> age) {
 		if (name == "Nosurname" && age == 0) break;
-		Name_value p;
-		p.set(name, age);
+		Name_value p(name, age);
 		for (Name_value x : v) {
 			if (x.getName() == name) error("We have duplicate in names.");
 		}
@@ -35,9 +31,13 @@ int main() {
 	for (Name_value x : v) {
 		cout << x.getName() << " " << x.getAge() << endl;
 	}
+	return v;
+}
 
+void additional_acctions(const vector<Name_value> &v) {
+	string name = "";
+	int age = 0;
 	bool next = false;
-	name = "";
 	cout << "Do you want check some names?" << endl;
 	cin >> next;
 	if (next) {
@@ -59,9 +59,8 @@ int main() {
 		}
 	}
 	else {
-		return 0;
+		return;
 	}
-	age = 0;
 	cout << "Do you want check some ages?" << endl;
 	cin >> next;
 	if (next) {
@@ -83,75 +82,16 @@ int main() {
 		}
 	}
 	else {
-		return 0;
+		return;
 	}
+}
 
-
-
-
-	/*vector<string> names;
-	vector<int> ages;
-	string temp_s;
-	int temp_i;
-	while (cin >> temp_s >> temp_i) {
-		if (temp_s == "NoSurname" && temp_i == 0) {
-			break;
-		}
-		names.push_back(temp_s);
-		ages.push_back(temp_i);
+int main() {
+	try {
+		vector<Name_value> v = populate_vector();
+		additional_acctions(v);
 	}
-	for (int i = 0; i < names.size(); ++i) {
-		cout << names[i] << " " << ages[i] << endl;
+	catch (exception& e) {
+		cout << e.what() << endl;
 	}
-	bool next = false;
-	string name = "";
-	cout << "Do you want check some names?" << endl;
-	cin >> next;
-	if (next) {
-		while (cin >> name) {
-			if (name == "NoName") {
-				break;
-			}
-			int temp = 0;
-			for (int i = 0; i < names.size(); ++i) {
-				if (name == names[i]) {
-					temp = ages[i];
-				}
-			}
-			if (temp != 0) {
-				cout << temp << endl;
-			}
-			else {
-				cout << "Name not found." << endl;
-			}
-		}
-	}
-	else {
-		return 0;
-	}
-	int age = 0;
-	cout << "Do you want check some names?" << endl;
-	cin >> next;
-	if (next) {
-		while (cin >> age) {
-			if (age == -1) {
-				break;
-			}
-			string temp = "";
-			for (int i = 0; i < ages.size(); ++i) {
-				if (age == ages[i]) {
-					temp = names[i];
-				}
-			}
-			if (temp != "") {
-				cout << temp << endl;
-			}
-			else {
-				cout << "Age not found." << endl;
-			}
-		}
-	}
-	else {
-		return 0;
-	}*/
 }
