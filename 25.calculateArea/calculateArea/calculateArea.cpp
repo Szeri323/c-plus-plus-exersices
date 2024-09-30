cda8d57270aa3a4ba1ca94044145acebba69dd75
Szeri <szeri323@gmail.com>
2024-09-30 19:17:54 +0200

    clear calculate area code

--- 25.calculateArea/calculateArea/calculateArea.cpp
+++ 25.calculateArea/calculateArea/calculateArea.cpp
@@ -20,38 +20,35 @@ double hexagon_area(double sides_length) {
 int main() {
 	int sides;
 	bool sides_equal;
-	while (cout << "How many sides? " << endl, cin >> sides, cout << "Sides are equal? (0,1)" << endl, cin >> sides_equal)
-		if (sides_equal == true) {
-			if (sides == 4) {
-				double sides_length;
-				cout << "Pass sides length: " << endl;
-				cin >> sides_length;
-				try {
+	while (cout << "How many sides? " << endl, cin >> sides, cout << "Sides are equal? (0,1)" << endl, cin >> sides_equal) {
+		try {
+			if (sides_equal == true) {
+				if (sides == 4) {
+					double sides_length;
+					cout << "Pass sides length: " << endl;
+					cin >> sides_length;
 					cout << "Square area is: " << square_area(sides_length) << endl;
 				}
-				catch (runtime_error& e) {
-					cout << e.what() << endl;
+				if (sides == 6) {
+					double sides_length;
+					cout << "Pass sides length: " << endl;
+					cin >> sides_length;
+					cout << "Hexagon area is: " << hexagon_area(sides_length) << endl;
 				}
 			}
-			if (sides == 6) {
-				double sides_length;
-				cout << "Pass sides length: " << endl;
-				cin >> sides_length;
-				cout << "Hexagon area is: " << hexagon_area(sides_length) << endl;
-			}
-		}
-		else {
-			if (sides == 4) {
-				int length;
-				int width;
-				cout << "Pass length and width: " << endl;
-				cin >> length >> width;
-				try {
+			else {
+				if (sides == 4) {
+					int length;
+					int width;
+					cout << "Pass length and width: " << endl;
+					cin >> length >> width;
 					cout << "Rectangle area is: " << rectangle_area(length, width) << endl;
-				}
-				catch (runtime_error& e) {
-					cout << e.what() << endl;
+
 				}
 			}
 		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+	}
 }
