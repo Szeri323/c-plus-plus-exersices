6aa4e19d0824eb9a1f0c7193d3e726b63b4dc21c
Szeri <szeri323@gmail.com>
2024-09-30 13:16:53 +0200

    add try catch for dominant, remove unnessesary functions

--- 19.dominant/dominant/dominant.cpp
+++ 19.dominant/dominant/dominant.cpp
@@ -1,23 +1,5 @@
 #include "../../../headers/std_lib_facilities.h"
 
-bool numberInVector(vector<int> numbers, int number) {
-	for (int i = 0; i < numbers.size(); ++i) {
-		if (number == numbers[i]) {
-			return true;
-		}
-	}
-	return false;
-}
-
-bool stringInVector(vector<string> words, string word) {
-	for (int i = 0; i < words.size(); ++i) {
-		if (word == words[i]) {
-			return true;
-		}
-	}
-	return false;
-}
-
 int indexOfVectorMax(vector<int> numbers) {
 	int max = 0;
 	int index = 0;
@@ -61,77 +43,103 @@ void printStringVector(vector<string> words) {
 		cout << x << " ";
 }
 
-int main() {
-	/*vector<int> numbers{ 1,2,3,3,3,5,5,5,5,5,5,5,5,5,3,3,2,1,1,2 };
-	vector<int> unique, counters;
-	int counter = 0;
-	int number = numbers[0];
-	sort(numbers.begin(), numbers.end());
-	for (int i = 0; i < numbers.size(); ++i) {
-		if (unique.size() == 0) {
-			unique.push_back(number);
-		}
-		if (numbers[i] != number) {
-			number = numbers[i];
-			if (!numberInVector(unique, number)) {
+bool get_user_choice() {
+	bool choice;
+	cout << "Do you want to count dominant for words(0) or numbers(1):";
+	cin >> choice;
+	return choice;
+}
+
+void calculate_dominant(bool choice) {
+	if (choice == true) {
+		vector<int> numbers{ 9,1,2,3,3,3,5,5,5,5,9,5,5,5,5,5,3,3,2,1,1,2,9};
+		vector<int> unique, counters;
+		int counter = 0;
+		sort(numbers.begin(), numbers.end());
+		int number = numbers[0];
+		for (int i = 0; i < numbers.size(); ++i) {
+			if (unique.size() == 0) {
 				unique.push_back(number);
 			}
-			counters.push_back(counter);
-			counter = 1;
-		}
-		else {
-			++counter;
-		}
-		if (i == numbers.size() - 1) {
-			counters.push_back(counter);
-		}
-
-	}
-	cout << "Numbers: ";
-	printIntVector(numbers);
-	cout << endl;
-	cout << "Unique numbers: ";
-	printIntVector(unique);
-	cout << endl;
-	cout << "Counters of occurrences in numbers vector: ";
-	printIntVector(counters);
-	cout << endl;
-	cout << "Dominant: " << unique[indexOfVectorMax(counters)] << endl;*/
-	
-	vector<string> words{ "test","mario", "test", "test", "wyrewolwerowany", "analogicznie", "analogicznie", "analogicznie", "analogicznie", "analogicznie", "test", "everest"};
-	vector<string> unique;
-	vector<int> counters;
-	int counter = 1;
-	string word = words[0];
-	sort(words.begin(), words.end());
-	for (int i = 0; i < words.size(); ++i) {
-		if (unique.size() == 0) {
-			unique.push_back(word);
-		}
-		if (words[i] != word) {
-			word = words[i];
-			if (!stringInVector(unique, word)) {
-				unique.push_back(word);
+			if (numbers[i] != number) {
+				number = numbers[i];
+				unique.push_back(number);
+				counters.push_back(counter);
+				counter = 1;
 			}
-			counters.push_back(counter);
-			counter = 1;
-		}
-		else {
-			++counter;
-		}
+			else {
+				++counter;
+			}
+			if (i == numbers.size() - 1) {
+				counters.push_back(counter);
+			}
 
+		}
+		cout << "Numbers: ";
+		printIntVector(numbers);
+		cout << endl;
+		cout << "Unique numbers: ";
+		printIntVector(unique);
+		cout << endl;
+		cout << "Counters of occurrences in numbers vector: ";
+		printIntVector(counters);
+		cout << endl;
+
+		cout << "Min: " << minOfVector(counters) << endl;
+		cout << "Max: " << maxOfVector(counters) << endl;
+		cout << "Dominant: " << unique[indexOfVectorMax(counters)] << endl;
 	}
-	cout << "Words: ";
-	printStringVector(words);
-	cout << endl;
-	cout << "Unique words: ";
-	printStringVector(unique);
-	cout << endl;
-	cout << "Counters of occurrences in words vector: ";
-	printIntVector(counters);
-	cout << endl;
+	else {
+		vector<string> words{ "test","mario", "test", "test", "wyrewolwerowany", "wyrewolwerowany", "analogicznie", "analogicznie", "analogicznie", "analogicznie", "analogicznie", "test", "everest" };
+		vector<string> unique;
+		vector<int> counters;
+		int counter = 0;
+		sort(words.begin(), words.end());
+		string word = words[0];
+		cout << endl;
+		cout << endl;
+		printStringVector(words);
+		cout << endl;
+		cout << endl;
 
-	cout << "Min: " << minOfVector(counters) << endl;
-	cout << "Max: " << maxOfVector(counters) << endl;
-	cout << "Dominant: " << unique[indexOfVectorMax(counters)] << endl;
+
+		for (int i = 0; i < words.size(); ++i) {
+			if(unique.size() == 0)
+				unique.push_back(words[i]);
+			if(words[i] != word) {
+				word = words[i];
+				unique.push_back(words[i]);
+				counters.push_back(counter);
+				counter = 1;
+			}
+			else
+				++counter;
+			if (i == words.size() - 1) {
+				counters.push_back(counter);
+			}
+		}
+		cout << "Words: ";
+		printStringVector(words);
+		cout << endl;
+		cout << "Unique words: ";
+		printStringVector(unique);
+		cout << endl;
+		cout << "Counters of occurrences in words vector: ";
+		printIntVector(counters);
+		cout << endl;
+
+		cout << "Min: " << minOfVector(counters) << endl;
+		cout << "Max: " << maxOfVector(counters) << endl;
+		cout << "Dominant: " << unique[indexOfVectorMax(counters)] << endl;
+	}
+}
+
+int main() {
+	try {
+		bool choice = get_user_choice();
+		calculate_dominant(choice);
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
 }
