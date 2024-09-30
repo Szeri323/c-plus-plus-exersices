d0d789b86e3d48fd0435621f7f9028be7f107599
Szeri <szeri323@gmail.com>
2024-09-30 19:02:24 +0200

    fix class name for temps

--- 24.temps/temps/temps.cpp
+++ 24.temps/temps/temps.cpp
@@ -1,8 +1,8 @@
 #include "../../../headers/std_lib_facilities.h"
 
-class Temp {
+class Temps {
 public:
-    Temp() : high_temp(-1000), low_temp(1000), sum(0), number_of_temps(0) {}
+    Temps() : high_temp(-1000), low_temp(1000), sum(0), number_of_temps(0) {}
     void set_values(double temp);
     double get_high_temp();
     double get_low_temp();
@@ -15,30 +15,30 @@ private:
     int number_of_temps;
 };
 
-void Temp::set_values(double temp) {
+void Temps::set_values(double temp) {
     ++number_of_temps;
     if (temp > high_temp) high_temp = temp;
     if (temp < low_temp) low_temp = temp;
     sum += temp;
 }
 
-double Temp::get_high_temp() {
+double Temps::get_high_temp() {
     return high_temp;
 }
 
-double Temp::get_low_temp() {
+double Temps::get_low_temp() {
     return low_temp;
 }
 
-double Temp::get_sum() {
+double Temps::get_sum() {
     return sum;
 }
 
-int Temp::get_number_of_temps() {
+int Temps::get_number_of_temps() {
     return number_of_temps;
 }
 
-void get_temps(Temp &t) {
+void get_temps(Temps &t) {
     int temp = 0;
     while (cin >> temp) {
         t.set_values(temp);
@@ -46,7 +46,7 @@ void get_temps(Temp &t) {
     
 }
 
-void print_results(Temp t) {
+void print_results(Temps t) {
     cout << "Highest temperature : " << t.get_high_temp() << endl;
     cout << "Lowest temperature : " << t.get_low_temp() << endl;
     cout << "Average temperature : " << t.get_sum() / t.get_number_of_temps() << endl;
@@ -54,7 +54,7 @@ void print_results(Temp t) {
 
 int main() {
     try {
-        Temp t;
+        Temps t;
         get_temps(t);
         print_results(t);
     }
