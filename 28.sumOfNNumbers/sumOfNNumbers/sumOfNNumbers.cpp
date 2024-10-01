49c4b543631a5c3535bfd0b4fb7a8bdc1ce3cbef
Szeri <szeri323@gmail.com>
2024-10-01 12:18:11 +0200

    add const references to sum of n numbers

--- 28.sumOfNNumbers/sumOfNNumbers/sumOfNNumbers.cpp
+++ 28.sumOfNNumbers/sumOfNNumbers/sumOfNNumbers.cpp
@@ -3,27 +3,21 @@
 vector<double> populate_vector() {
 	vector<double> numbers;
 	double number;
-	cout << "Pass sequence of numbers:" << endl;
 	while (cin >> number) {
 		numbers.push_back(number);
 	}
 	cin.clear();
+	if (numbers.size() == 0) error("No numbers to process.");
 	return numbers;
 }
 
 int get_number() {
-	int n;
-	cout << "How many first numbers do you want to sum: ";
+	int n = 0;
 	cin >> n;
-	if (n < 0) error("Number can't be negative");
-	if (n == 0) {
-		cout << "No numbers will be sum." << endl;
-		return 0;
-	}
 	return n;
 }
 
-double sum_vector_numebrs(vector<double> numbers, int n) {
+double sum_vector_numebrs(const vector<double> &numbers, int n) {
 	double sum = 0;
 	for (int i = 0; i < n; ++i) {
 		sum += numbers[i];
@@ -31,7 +25,7 @@ double sum_vector_numebrs(vector<double> numbers, int n) {
 	return sum;
 }
 
-vector<double> calculate_difference_between_numebrs(vector<double> numbers) {
+vector<double> calculate_difference_between_numebrs(const vector<double> &numbers) {
 	vector<double> differences;
 	for (int i = 1; i < numbers.size(); ++i) {
 		differences.push_back(numbers[i - 1] - numbers[i]);
@@ -39,7 +33,7 @@ vector<double> calculate_difference_between_numebrs(vector<double> numbers) {
 	return differences;
 }
 
-void print_results(double sum, int n, vector<double> differences) {
+void print_results(double sum, int n, const vector<double> &differences) {
 	cout << "Sum of " << n << " first numbers is: " << sum << endl;
 	cout << "Differences of vector numbers are:" << endl;
 	for (double x : differences) {
@@ -49,9 +43,12 @@ void print_results(double sum, int n, vector<double> differences) {
 
 int main() {
 	try {
+		cout << "Pass sequence of numbers:" << endl;
 		vector<double> numbers = populate_vector();
+		cout << "How many first numbers do you want to sum: ";
 		int n = get_number();
-		if (numbers.size() < n) error("Not enough numbers to sum.");
+		if (n < 0) error("Number can't be negative");
+		if (n > numbers.size()) error("Not enough numbers to sum.");
 		int sum = sum_vector_numebrs(numbers, n);
 		vector<double> differences = calculate_difference_between_numebrs(numbers);
 		print_results(sum, n, differences);
