b40530273be977e14748969039c4ae9917b59d9f
Szeri <szeri323@gmail.com>
2024-10-03 15:48:56 +0200

    add headers include practice

---
+++ my/my/my.cpp
@@ -0,0 +1,12 @@
+#include "my.h"
+#include "../../../headers/std_lib_facilities.h"
+
+int foo = 0;
+
+void print(int i) {
+	cout << "Print(int i): " << i << endl;
+}
+
+void print_foo() {
+	cout << "Print_foo: " << foo << endl;
+}
