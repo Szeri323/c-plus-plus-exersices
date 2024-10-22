cd50ecd1f3f256da41c5597f238e26a2ad34dff2
Szeri <szeri323@gmail.com>
2024-10-22 13:04:14 +0200

    update code struct of date struct

--- dateClasses/dateClasses/dateStruct.cpp
+++ dateClasses/dateClasses/dateStruct.cpp
@@ -1,56 +1,32 @@
 #include "./dateStruct/dateStruct.h"
 #include "../../../headers/std_lib_facilities.h"
 
+bool is_leapyear(int y, int m, int d) {
+	if (y % 4 == 0 && y % 100 != 0 && y % 400 != 0 || y % 4 == 0 && y % 100 == 0 && y % 400 == 0) {
+		return true;
+	}
+	return false;
+}
+
+int set_max_days_for_month(int m, bool leapyear) {
+	switch (m) {
+	case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+		return 31;
+	case 4:	case 6: case 9: case 11:
+		return 30;
+	case 2:
+		if (leapyear)
+			return 29;
+		else
+			return 28;
+	}
+}
+
 void init_day(Date& dd, int y, int m, int d) {
 	// checks if date contains correct year, month and day
-	if (y % 4 == 0 && y % 100 != 0 && y % 400 != 0 || y % 4 == 0 && y % 100 == 0 && y % 400 == 0) {
-		dd.leapyear = true;
-	}
-	else {
-		dd.leapyear = false;
-	}
-	switch (m) {
-	case 1:
-		dd.max_d = 31;
-		break;
-	case 2:
-		if (dd.leapyear)
-			dd.max_d = 29;
-		else
-			dd.max_d = 28;
-		break;
-	case 3:
-		dd.max_d = 31;
-		break;
-	case 4:
-		dd.max_d = 30;
-		break;
-	case 5:
-		dd.max_d = 31;
-		break;
-	case 6:
-		dd.max_d = 30;
-		break;
-	case 7:
-		dd.max_d = 31;
-		break;
-	case 8:
-		dd.max_d = 31;
-		break;
-	case 9:
-		dd.max_d = 30;
-		break;
-	case 10:
-		dd.max_d = 31;
-		break;
-	case 11:
-		dd.max_d = 30;
-		break;
-	case 12:
-		dd.max_d = 31;
-		break;
-	}
-
+	dd.leapyear = is_leapyear(y, m, d);
+	dd.max_d = set_max_days_for_month(m, dd.leapyear);
+	
 	if (y >= 0 && m >= 1 && m <= 12 && d >= 1 && d <= dd.max_d) {
 		dd.y = y;
 		dd.m = m;
--- dateClasses/dateClasses/dateStruct/dateStruct_test.h
+++ dateClasses/dateClasses/dateStruct/dateStruct_test.h
@@ -1,6 +1,2 @@
 #pragma once
-//void today_test(int y, int m, int d);
-//void tomorrow_test(int y, int m, int d);
-//void next_month_test(int y, int m, int d);
-//void next_year_test(int y, int m, int d);
 void tests();
